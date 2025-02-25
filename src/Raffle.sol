c9334d4e8e8bebdd4495ae1d0dcd25ebc327ee85
akasharora963 <akasharora963@gmail.com>
2025-02-25 22:03:04 +0530

    add: chainlink vrf and some logic variables

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier:MIT
 pragma solidity ^0.8.28;
 
+import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
 /**
  * @title Raffle
  * @author Akash Arora
@@ -8,25 +11,46 @@ pragma solidity ^0.8.28;
  * @notice This contrcat is for implementing Raffle
  */
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2Plus {
     /** Errors */
     error Raffle__NotEnoughFunds();
+    error Raffle__NotOver();
+    error Raffle__TransferFailed();
 
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entryFee;
+    uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
     address payable[] private s_players;
-
+    uint256 private s_lastTimeStamp;
+    address private s_lastWinner;
     /** Events */
     event RaffleJoined(address indexed player);
 
-    constructor(uint256 _entryFee) {
+    constructor(
+        uint256 _entryFee,
+        uint256 _interval,
+        address _vrfCoordinator,
+        bytes32 _gasLane,
+        uint256 _subscriptionId,
+        uint32 _callbackGasLimit
+    ) VRFConsumerBaseV2Plus(_vrfCoordinator) {
         i_entryFee = _entryFee;
+        i_interval = _interval;
+        i_keyHash = _gasLane;
+        i_subscriptionId = _subscriptionId;
+        i_callbackGasLimit = _callbackGasLimit;
+        s_lastTimeStamp = block.timestamp;
     }
 
     /**
      * @dev This function is for joining the raffle
      * @notice This function is for joining the raffle
      */
-    function joinRaffle() public payable {
+    function joinRaffle() external payable {
         if (msg.value < i_entryFee) {
             revert Raffle__NotEnoughFunds();
         }
@@ -37,9 +61,47 @@ contract Raffle {
     }
     /**
      * @dev This function is for choosing the winner of the raffle
+            Generate a randome number
+            Pick a random winner
+            Automatic calling
      * @notice This function is for choosing the winner of the raffle
      */
-    function chooseWinner() public {}
+    function chooseWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert Raffle__NotOver();
+        }
+        // Will revert if subscription is not set and funded.
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
+            .RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_callbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            });
+
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+
+        s_lastTimeStamp = block.timestamp;
+    }
+
+    function fulfillRandomWords(
+        uint256 requestId,
+        uint256[] calldata randomWords
+    ) internal virtual override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_lastWinner = winner;
+        (bool success, ) = winner.call{value: address(this).balance}("");
+
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+    }
 
     /**Getters */
     function getEntryFee() public view returns (uint256) {
