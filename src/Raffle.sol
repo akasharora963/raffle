6c769179b40ac5423314cfc63234732dc256e67f
akasharora963 <akasharora963@gmail.com>
2025-02-27 14:27:47 +0530

    update: format code

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
-import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
+import {AutomationCompatibleInterface} from
+    "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
 
 /**
  * @title Raffle
@@ -11,24 +12,26 @@ import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/autom
  * @dev implements Chainlink VRF
  * @notice This contrcat is for implementing Raffle
  */
-
 contract Raffle is VRFConsumerBaseV2Plus, AutomationCompatibleInterface {
-    /** Errors */
+    /**
+     * Errors
+     */
     error Raffle__NotEnoughFunds();
-    error Raffle__UpkeepNotNeeded(
-        uint256 balance,
-        uint256 playersLength,
-        uint256 raffleState
-    );
+    error Raffle__UpkeepNotNeeded(uint256 balance, uint256 playersLength, uint256 raffleState);
     error Raffle__TransferFailed();
     error Raffle__NotOpen();
 
-    /** Type Declarations */
+    /**
+     * Type Declarations
+     */
     enum RaffleState {
         OPEN,
         CALCULATING
     }
-    /** State Variables */
+    /**
+     * State Variables
+     */
+
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entryFee;
@@ -41,7 +44,9 @@ contract Raffle is VRFConsumerBaseV2Plus, AutomationCompatibleInterface {
     address private s_lastWinner;
     RaffleState private s_raffleState;
 
-    /** Events */
+    /**
+     * Events
+     */
     event RaffleJoined(address indexed player);
     event WinnerChosen(address indexed player);
 
@@ -89,64 +94,51 @@ contract Raffle is VRFConsumerBaseV2Plus, AutomationCompatibleInterface {
      * 3. The contract has ETH.
      * 4. Implicity, your subscription is funded with LINK.
      */
-    function checkUpkeep(
-        bytes memory /* checkData */
-    )
+    function checkUpkeep(bytes memory /* checkData */ )
         public
         view
         override
-        returns (bool upkeepNeeded, bytes memory /*performData*/)
+        returns (bool upkeepNeeded, bytes memory /*performData*/ )
     {
         bool hasTimePassed = (block.timestamp - s_lastTimeStamp) >= i_interval;
         bool isRaffleOpen = s_raffleState == RaffleState.OPEN;
         bool hasBalance = address(this).balance > 0;
         bool hasPlayers = s_players.length > 0;
-        upkeepNeeded =
-            hasTimePassed &&
-            isRaffleOpen &&
-            hasBalance &&
-            hasPlayers;
+        upkeepNeeded = hasTimePassed && isRaffleOpen && hasBalance && hasPlayers;
 
         return (upkeepNeeded, new bytes(0));
     }
     /**
      * @dev This function is for choosing the winner of the raffle
-            Generate a randome number
-            Pick a random winner
-            Automatic calling
+     *         Generate a randome number
+     *         Pick a random winner
+     *         Automatic calling
      * @notice This function is for choosing the winner of the raffle
      */
-    function performUpkeep(bytes calldata /* performData */) external {
-        (bool upkeepNeeded, ) = checkUpkeep("");
+
+    function performUpkeep(bytes calldata /* performData */ ) external {
+        (bool upkeepNeeded,) = checkUpkeep("");
         if (!upkeepNeeded) {
-            revert Raffle__UpkeepNotNeeded(
-                address(this).balance,
-                s_players.length,
-                uint256(s_raffleState)
-            );
+            revert Raffle__UpkeepNotNeeded(address(this).balance, s_players.length, uint256(s_raffleState));
         }
         s_raffleState = RaffleState.CALCULATING;
         // Will revert if subscription is not set and funded.
-        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient
-            .RandomWordsRequest({
-                keyHash: i_keyHash,
-                subId: i_subscriptionId,
-                requestConfirmations: REQUEST_CONFIRMATIONS,
-                callbackGasLimit: i_callbackGasLimit,
-                numWords: NUM_WORDS,
-                extraArgs: VRFV2PlusClient._argsToBytes(
-                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
-                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
-                )
-            });
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
 
         s_vrfCoordinator.requestRandomWords(request);
     }
 
-    function fulfillRandomWords(
-        uint256 /*requestId*/,
-        uint256[] calldata randomWords
-    ) internal virtual override {
+    function fulfillRandomWords(uint256, /*requestId*/ uint256[] calldata randomWords) internal virtual override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable winner = s_players[indexOfWinner];
         s_lastWinner = winner;
@@ -156,14 +148,16 @@ contract Raffle is VRFConsumerBaseV2Plus, AutomationCompatibleInterface {
 
         emit WinnerChosen(s_lastWinner);
 
-        (bool success, ) = winner.call{value: address(this).balance}("");
+        (bool success,) = winner.call{value: address(this).balance}("");
 
         if (!success) {
             revert Raffle__TransferFailed();
         }
     }
 
-    /**Getters */
+    /**
+     * Getters
+     */
     function getEntryFee() public view returns (uint256) {
         return i_entryFee;
     }
